123c7e55d4c33f01ec13852e90016e5088602ad3
林锦骏 <673115816@qq.com>
2021-03-05 21:54:46 +0800

    upcase mv

---
+++ src/pages/Artist/MV.jsx
@@ -0,0 +1,48 @@
+import React, {
+  useState, useEffect, useRef, memo,
+} from 'react';
+import dayjs from 'dayjs';
+import { Link } from 'react-router-dom';
+import { transPlayCount } from '@/common/utils';
+import { apiArtistMV } from '@/api';
+import useInfinite from '@/custom/useInfinite';
+import DomGridVideo from '@/components/GridVideo';
+
+export default memo(({ id }) => {
+  const [MVs, setMVs] = useState([]);
+  const offset = useRef(0);
+  const domScroll = useRef();
+  const domObserver = useRef();
+  const limit = 24;
+  const handleInit = async () => {
+    try {
+      const { mvs, time } = await apiArtistMV({
+        id,
+        limit,
+        offset: offset.current,
+      });
+      offset.current += limit;
+      setMVs((prev) => [...prev, ...mvs]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const {
+    handleIo,
+    handleUnIo,
+  } = useInfinite(handleInit, domScroll, domObserver);
+
+  useEffect(() => {
+    // handleInit();
+    handleIo();
+    return () => {
+      handleUnIo();
+    };
+  }, []);
+  return (
+    <div className="domArtist_section p-8">
+      <DomGridVideo list={MVs} type="mv" options={{ src: 'imgurl', duration: 'duration' }} />
+      <div ref={domObserver} />
+    </div>
+  );
+});
---
+++ src/pages/Video/MVList/index.jsx
@@ -0,0 +1,205 @@
+import React, {
+  useEffect, useState, memo, useMemo,
+
+} from 'react';
+// import { unstable_batchedUpdates } from 'react-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
+import {
+  IconChevronRight,
+} from '@tabler/icons';
+import {
+  apiMVFirst,
+  apiMVAll,
+  apiMVExclusiveRcmd,
+  apiTopMV,
+} from '@/api';
+import DomGridVideo from '@/components/GridVideo';
+import DomGridMVToplist from '@/components/GridMVToplist';
+import DomLoading from '@/components/Loading';
+import {
+  setMVlistInit, setMVlistTopareaInit,
+  setMVlistFirstareaInit,
+} from '@/reducers/mvlist/actions';
+import './style.scss';
+
+const category = [
+  '内地',
+  '港台',
+  '欧美',
+  '日本',
+  '韩国',
+];
+
+export default memo(() => {
+  console.log('mv');
+  const dispatch = useDispatch();
+  const {
+    firstArea,
+    topArea,
+    first,
+    hot,
+    wy,
+    top,
+    timestamp,
+  } = useSelector(({ mvlist }) => mvlist);
+  const memoLoading = useMemo(() => Date.now() - timestamp > 3600000, [timestamp]);
+  const handleInit = async () => {
+    try {
+      const [
+        { data: hot },
+        { data: wy },
+        { data: first },
+        { data: top },
+      ] = await Promise.all([
+        apiMVAll({
+          order: '最热',
+          limit: 6,
+        }),
+        apiMVExclusiveRcmd({
+          limit: 6,
+        }),
+        apiMVFirst({
+          area: firstArea,
+          limit: 6,
+        }),
+        apiTopMV({
+          area: topArea,
+          limit: 10,
+        }),
+      ]);
+      dispatch(setMVlistInit({
+        first,
+        hot,
+        wy,
+        top,
+        timestamp: Date.now(),
+      }));
+      // unstable_batchedUpdates(() => {
+      //   setMVHot(hot);
+      //   setMVWy(wy);
+      //   setMVFirst(first);
+      //   setMVTop(top);
+      //   setLoading(false);
+      // });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChangeFirstArea = async (firstArea) => {
+    try {
+      const { data: first } = await apiMVFirst({
+        area: firstArea,
+        limit: 6,
+      });
+      dispatch(setMVlistFirstareaInit({
+        firstArea,
+        first,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChangeTopArea = async (topArea) => {
+    try {
+      const { data: top } = await apiTopMV({
+        area: topArea,
+        limit: 10,
+      });
+      dispatch(setMVlistTopareaInit({
+        topArea,
+        top,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (memoLoading) {
+      handleInit();
+    }
+  }, [memoLoading]);
+
+  if (memoLoading) return <div className="w-full h-full flex-center"><DomLoading /></div>;
+  return (
+    <div className="domVideoList_content overflow-auto max-h-full flex-auto">
+      <div className="domMVList_sublist">
+        <div className="domMVList_header mb-5 flex justify-between items-center">
+          <Link
+            to={`/allmv?order=最新&area=${firstArea}`}
+            className="domMVList_subLink font-bold text-base flex items-center"
+          >
+            最新MV
+            <IconChevronRight size={20} />
+          </Link>
+          <div className="recommend_nav flex divide-x space-x-1">
+            {
+              category.map((item) => (
+                <div className="item" key={item}>
+                  <button
+                    onClick={() => handleChangeFirstArea(item)}
+                    type="button"
+                    className={classnames('link rounded-full px-2.5', firstArea === item ? 'text-red-500 bg-red-50' : 'ui_text_black_hover')}
+                  >
+                    {item}
+                  </button>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <DomGridVideo list={first} type="mv" />
+      </div>
+      <div className="domMVList_sublist">
+        <div className="domMVList_header mt-8 mb-5 flex justify-between items-center">
+          <Link
+            to="/allmv?order=最热"
+            className="domMVList_subLink font-bold text-base flex items-center"
+          >
+            热播MV
+            <IconChevronRight size={20} />
+          </Link>
+        </div>
+        <DomGridVideo list={hot} type="mv" />
+      </div>
+      <div className="domMVList_sublist">
+        <div className="domMVList_header mt-8 mb-5 flex justify-between items-center">
+          <Link
+            to="/allmv?type=网易出品&order=最新"
+            className="domMVList_subLink font-bold text-base flex items-center"
+          >
+            网易出品
+            <IconChevronRight size={20} />
+          </Link>
+        </div>
+        <DomGridVideo list={wy} type="mv" />
+      </div>
+      <div className="domMVList_header mt-8 mb-5 flex justify-between items-center">
+        <Link to="/toplist-mv/" className="domMVList_subLink font-bold text-base flex items-center">
+          MV排行榜
+          <IconChevronRight size={20} />
+        </Link>
+        <div className="recommend_nav flex divide-x space-x-1">
+          {
+            category.map((item) => (
+              <div className="item" key={item}>
+                <button
+                  onClick={() => handleChangeTopArea(item)}
+                  type="button"
+                  className={classnames('link rounded-full px-2.5', topArea === item ? 'text-red-500 bg-red-50' : 'ui_text_black_hover')}
+                >
+                  {item}
+                </button>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+      <DomGridMVToplist list={top} />
+    </div>
+  );
+});
