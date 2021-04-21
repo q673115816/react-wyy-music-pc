add4d26a8f2d1897181716fa5f9aed35e8fedc82
林锦骏 <673115816@qq.com>
2021-04-21 17:27:41 +0800

    add arrow left right

--- src/custom/useKeyActions.jsx
+++ src/custom/useKeyActions.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { setAudioRunningToggle } from '@/reducers/audio/actions';
+import { setAudioRunningToggle, setAudioNext, setAudioPrev } from '@/reducers/audio/actions';
 import { setVolumePlusTen, setVolumeSubTen } from '@/reducers/volume/actions';
 
 export default () => {
@@ -8,13 +8,17 @@ export default () => {
   // console.log('keyActions');
   const keyDown = (e) => {
     const { ctrlKey, keyCode } = e;
-    // console.log(keyCode);
+    console.log(keyCode);
     if (ctrlKey) {
       switch (keyCode) {
         case 38:// 音量
           return dispatch(setVolumePlusTen());
         case 40:// 音量
           return dispatch(setVolumeSubTen());
+        case 37:
+          return dispatch(setAudioPrev());
+        case 39:
+          return dispatch(setAudioNext());
         case 80:
           e.preventDefault();// 原为打印
           return dispatch(setAudioRunningToggle());
