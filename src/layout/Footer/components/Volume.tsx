e2f590ee4fb1a6cd331ed42ee64f1fe15e776375
林锦骏 <673115816@qq.com>
2021-07-26 18:11:55 +0800

    update volume

--- src/layout/Footer/components/Volume.tsx
+++ src/layout/Footer/components/Volume.tsx
@@ -4,23 +4,87 @@ import { IconVolume, IconVolume3 } from "@tabler/icons";
 import { setVolume, setBeforeMuted } from "@/reducers/volume/slice";
 import { computedPositionPercentage } from "@/common/utils";
 import Drag from "@/components/Drag";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  memo,
+  useMemo,
+  PropsWithChildren
+} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  IconVolume,
+  IconVolume3,
+} from '@tabler/icons';
+import { setVolume, setBeforeMuted } from '@/reducers/volume/actions';
+import {computedPositionPercentage, isInTheRect} from '@/common/utils';
+import { setDragInit, setDragUnload } from '@/reducers/drag/actions';
 
-export default memo(() => {
-  const dispatch = useAppDispatch();
-  const { volume, beforeMuted } = useAppSelector(({ volume }) => volume);
-  const RefProgress = useRef<HTMLDivElement>(null);
-  const [muted, setMuted] = useState(() => volume === 0);
+const Ico = memo<PropsWithChildren<{isMuted: boolean}>>(({ isMuted }) => (
+  isMuted ?
+    <IconVolume3 size={28} stroke={1} /> :
+    <IconVolume size={28} stroke={1} />
+))
+
+
+const Panel = memo<PropsWithChildren<{ visible: boolean }>>(({visible, setPanelVisible}) => {
+  const dispatch = useDispatch();
+  const { volume } = useSelector(({ volume }) => volume);
   const [dragger, SetDragger] = useState(false);
-  const [active, setActive] = useState(false);
+  const RefRect = useRef<HTMLDivElement>(null)
+  const RefProgress = useRef<HTMLDivElement>(null);
 
-  const computedPosition = (e) => {
-    const percentage = computedPositionPercentage(e, RefProgress.current);
+  if(!(visible || dragger)) return null;
+  const computedPosition = (e: MouseEvent) => {
+    const {current: dom} = RefProgress
+    if(!dom) return 0
+    const percentage = computedPositionPercentage(e, dom, 'y');
     return percentage * 100;
   };
 
-  const handleClickSetVolume = (e) => {
+  const handleDropMove = (e: MouseEvent) => {
     dispatch(setVolume(computedPosition(e)));
   };
+
+  const onMouseUp = () => {
+    SetDragger(false);
+    const {current: dom} = RefRect
+    if(dom && isInTheRect(e, dom))
+      setPanelVisible(true)
+  };
+
+  const onMouseDown = () => {
+    SetDragger(true);
+  };
+
+  return (
+  <div className="w-8 h-24 flex-center absolute bottom-full bg-white rounded shadow" ref={RefRect}>
+    <div
+      className="bg-gray-200 flex flex-col h-20 justify-end w-1"
+      title="音量调节（Ctrl + Up / Ctrl + Down）"
+      onMouseDown={handleDropMove}
+      ref={RefProgress}
+    >
+      <div
+        className="curr width-full h-full ui_theme_bg_color relative"
+        style={{ height: `${volume}%` }}>
+        <div
+          onMouseDown={handleDropDown}
+          className="absolute right-1/2 top-0 transform translate-x-1/2 -translate-y-1/2 w-2 h-2 ui_theme_bg_color rounded-full"
+        />
+      </div>
+    </div>
+  </div>)
+})
+
+export default memo(() => {
+  const dispatch = useDispatch();
+  const { volume, beforeMuted } = useSelector(({ volume }) => volume);
+  const [muted, setMuted] = useState(() => volume === 0);
+
+  const [panelVisible, setPanelVisible] = useState(false)
+
 
   const handleMutedChange = () => {
     if (muted) {
@@ -31,17 +95,7 @@ export default memo(() => {
     }
   };
 
-  const onMouseMove = (e: MouseEvent) => {
-    dispatch(setVolume(computedPosition(e)));
-  };
-
-  const onMouseUp = () => {
-    SetDragger(false);
-  };
-
-  const onMouseDown = () => {
-    SetDragger(true);
-  };
+  const isMuted = useMemo(() => muted || volume === 0, [muted, volume])
 
   useEffect(() => {
     if (volume === 0) {
@@ -52,41 +106,21 @@ export default memo(() => {
   }, [volume]);
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className="relative flex-center group"
+         onMouseEnter={() => setPanelVisible(true)}
+         onMouseLeave={() => setPanelVisible(false)}
+    >
       <button
         type="button"
         className="volume_btn"
         title="静音/恢复音量"
         onClick={handleMutedChange}
       >
-        {muted || volume === 0 ? (
-          <IconVolume3 size={28} stroke={1} />
-        ) : (
-          <IconVolume size={28} stroke={1} />
-        )}
+        <Ico isMuted={isMuted} />
       </button>
-      <div
-        title="音量调节（Ctrl + Up / Ctrl + Down）"
-        ref={RefProgress}
-        onClick={handleClickSetVolume}
-        onMouseEnter={() => setActive(true)}
-        onMouseLeave={() => setActive(false)}
-        className="volume_value w-14 h-1 bg-gray-200"
-      >
-        <div
-          className="curr width-full h-full ui_theme_bg_color relative"
-          style={{ width: `${volume}%` }}
-        >
-          {(dragger || active) && (
-            <Drag
-              onMouseDown={onMouseDown}
-              onMouseMove={onMouseMove}
-              onMouseUp={onMouseUp}
-              className="absolute right-0 top-1/2 transform -translate-y-1/2 translate-x-1/2 w-2 h-2 ui_theme_bg_color rounded-full"
-            />
-          )}
-        </div>
-      </div>
+      {
+        <Panel visible={panelVisible} setPanelVisible={setPanelVisible}/>
+      }
     </div>
   );
 });
