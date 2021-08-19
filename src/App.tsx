a9284676f74745348d5f2d6b62aed597bb621b41
鸽蛋白 <673115816@qq.com>
2021-08-19 20:24:37 +0800

    simple

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {
   memo, useEffect, lazy, Suspense,
 } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import DomResizer from '@/components/Resizer';
+import Resizer from '@/components/Resizer';
 import useMemoParmas from '@/hooks/useMemoParams';
 import useIsLogin from '@/hooks/useIsLogin';
 import DomLoading from '@/components/Loading';
@@ -14,44 +14,14 @@ import useKeyActions from './hooks/useKeyActions';
 import './styles/index.scss';
 
 import GlobalLrc from './components/Lrc';
-import {useAppSelector} from "@/reducers/hooks";
+import Help from './Help'
+import { useAppSelector } from "@/reducers/hooks";
 
 const Player = lazy(() => import(
   /* webpackChunkName: "Player" */
   '@/pages/Player'));
-const Lrc = lazy(() => import(
-  /* webpackChunkName: "Lrc" */
-  '@/pages/Lrc'));
-const DialogLogin = lazy(() => import(
-  /* webpackChunkName: "Components_dialog_Login" */
-  '@/components/Dialog/Login'));
-const DialogShare = lazy(() => import(
-  /* webpackChunkName: "Components_dialog_Share" */
-  '@/components/Dialog/Share'));
-const DialogShareWX = lazy(() => import(
-  /* webpackChunkName: "Components_dialog_ShareWX" */
-  '@/components/Dialog/ShareWX'));
-const DialogCreatePlaylist = lazy(() => import(
-  /* webpackChunkName: "Components_dialog_CreatePlaylist" */
-  '@/components/Dialog/CreatePlaylist'));
-const DialogUnSubscription = lazy(() => import(
-  /* webpackChunkName: "Components_dialog_UnSubscription" */
-  '@/components/Dialog/UnSubscription'));
-const Playlist = lazy(() => import(
-  /* webpackChunkName: "Components_Playlist" */
-  '@/components/Playlist'));
-const Letter = lazy(() => import(
-  /* webpackChunkName: "Components_Playlist" */
-  '@/components/Letter'));
-const Toast = lazy(() => import(
-  /* webpackChunkName: "Components_Toast" */
-  '@/components/Toast'));
-const Contextmenu = lazy(() => import(
-  /* webpackChunkName: "Components_Contextmenu" */
-  '@/components/Contextmenu'));
-const HeaderSearch = lazy(() => import(
-  /* webpackChunkName: "Components_HeaderSearch" */
-  '@/components/Search/Search'));
+
+
 
 export default memo(() => {
   console.log('app')
@@ -69,39 +39,27 @@ export default memo(() => {
         '--themeColor': theme,
       }}
     >
-      <Router basename={PUBLIC_URL}>
-        <Inset>
-          <DomHeader />
-          <Suspense fallback={<div className="flex-center w-full h-full"><DomLoading /></div>}>
-            <Switch>
-              <Route
-                path="/player/:type(video|mv)/:vid"
-              >
-                {useMemoParmas(Player)}
-              </Route>
-              <Route>
-                <DomMain />
-                <DomFooter />
-              </Route>
-            </Switch>
-            <DialogShare />
-            <DialogShareWX />
-
-            <DialogCreatePlaylist />
-            <DialogUnSubscription />
-
-            <Lrc />
-            <Playlist />
-            <Letter />
-            <HeaderSearch />
-            <Contextmenu />
-            <Toast />
-            <DialogLogin />
-          </Suspense>
-
-          <DomResizer />
-        </Inset>
-      </Router>
+      <Inset>
+        <DomHeader />
+        <Suspense fallback={
+          <div className="flex-center w-full h-full">
+            <DomLoading />
+          </div>}>
+          <Switch>
+            <Route
+              path="/player/:type(video|mv)/:vid"
+            >
+              {useMemoParmas(Player)}
+            </Route>
+            <Route>
+              <DomMain />
+              <DomFooter />
+            </Route>
+          </Switch>
+          <Help />
+        </Suspense>
+        <Resizer />
+      </Inset>
       <GlobalLrc />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router} from 'react-router-dom'
 import store from './reducers/store';
 
 import App from './App';
@@ -9,9 +10,11 @@ import Issues from './Issues';
 
 render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router basename={PUBLIC_URL}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
