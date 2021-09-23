437f356835afd12f10117360c6d302404ee3178b
林锦骏 <673115816@qq.com>
2021-09-23 11:36:51 +0800

    fixed: ssr before

--- build/webpack.ssr.js
+++ build/webpack.ssr.js
@@ -1,16 +1,17 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const merge = require('webpack-merge');
-const base = require('./webpack.base');
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const merge = require("webpack-merge");
+const base = require("./webpack.base");
 
-const serverConf = {
-  mode: 'development',
-  target: 'node',
+const ssr = {
+  mode: "production",
+  target: "node",
   externals: [nodeExternals()],
   // entry: './src/server/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './build'),
+    clean: true,
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].[chunkhash:8].js",
   },
 };
-module.exports = merge(base, serverConf);
+module.exports = merge(base, ssr);
