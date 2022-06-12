f08e418373e9e424cbe28072a8dc550d49368126
673115816@qq.com <673115816@qq.com>
2022-06-13 00:32:46 +0800

    test: 💍 cicd preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,19 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: [
     // "plugin:react/recommended", "prettier",
+    // ESLint
     "eslint:recommended",
+    // TypeScript
+    "plugin:@typescript-eslint/recommended",
+    // Jest
+    "plugin:jest/recommended",
+    // React Testing Library
+    "plugin:testing-library/react",
+    // Prettier
     "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
