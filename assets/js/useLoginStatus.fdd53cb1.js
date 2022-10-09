adafbf9adbe2e00f26af02703f890b984c7eb8c5
q673115816 <673115816@qq.com>
2022-10-09 11:14:38 +0000

    Deploying to gh-pages from @ q673115816/react-wyy-music-pc@197586ed8f2460eb79cdc2fca6a5b617a8dc68a0 🚀

--- assets/js/useLoginStatus.fdd53cb1.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./react.0a2f695e.js";import{$ as r}from"./index.ad939b39.js";import{u as s}from"./react-router-dom.26906018.js";const e=async o=>{try{const{data:{profile:t}}=await r();o&&o(t)}catch(t){console.log(t)}},u=()=>{const o=s();a.exports.useEffect(()=>{e(t=>{t||o("/",{replace:!0})})},[])};export{u};
